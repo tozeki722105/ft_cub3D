bdb6a9df0774bb45f3abf1811a00a2e7efa71194
Toshihiro Ozeki <tozeki@c3r6s5.42tokyo.jp>
2024-07-18 06:54:29 +0900

    add config.h update calc.h ogv1.h parse.h utils.h の不要な依存関係を修正した

---
+++ includes/config.h
@@ -0,0 +1,15 @@
+#ifndef CONFIG_H
+# define CONFIG_H
+
+#define WINDOW_WIDTH	1000
+#define WINDOW_HEIGHT	500
+
+#define MAP_PANEL_SIDE	50
+#define	MAP_PADDING	10
+
+#define ANGLE_STEP	1
+#define MOVE_STEP	5
+
+#define FOV	60 
+
+#endif
