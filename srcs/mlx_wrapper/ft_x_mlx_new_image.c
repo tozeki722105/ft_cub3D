09dc6fb83be3a03df539a928bbd5748bb62fd373
taka-chin <tyamauch@c3r6s3.42tokyo.jp>
2024-08-19 16:42:24 +0900

    norm

--- srcs/mlx_wrapper/ft_x_mlx_new_image.c
+++ srcs/mlx_wrapper/ft_x_mlx_new_image.c
@@ -1,12 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_x_mlx_new_image.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tyamauch <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/19 16:40:06 by tyamauch          #+#    #+#             */
+/*   Updated: 2024/08/19 16:40:17 by tyamauch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 #include "mlx.h"
 #include "utils.h"
 
-void	*ft_x_mlx_new_image(t_xvar *xvar,int width, int height)
+void	*ft_x_mlx_new_image(t_xvar *xvar, int width, int height)
 {
 	void	*ret;
 
-	ret = mlx_new_image(xvar,width, height);
+	ret = mlx_new_image(xvar, width, height);
 	if (ret == NULL)
 		ft_my_perror_exit("mlx_new_image", 1);
 	return (ret);
