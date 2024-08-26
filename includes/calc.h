5e5953670fe6f76c3e9e81cffbb51b2cb00bdc2e
taka-chin <ty80164@gmail.com>
2024-08-26 20:06:40 +0900

    fix norm parseを除く

--- includes/calc.h
+++ includes/calc.h
@@ -1,12 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   calc.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tyamauch <tyamauch@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/26 19:42:55 by tyamauch          #+#    #+#             */
+/*   Updated: 2024/08/26 19:43:00 by tyamauch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef CALC_H
 # define CALC_H
 
-#include <math.h>
-#include "main.h"
-#include "draw.h"
-#include "../library/libft/libft.h"
+# include "../library/libft/libft.h"
+# include "draw.h"
+# include "main.h"
+# include <math.h>
 
-enum	e_axis
+enum			e_axis
 {
 	VERTICAL,
 	HORIZONTAL,
@@ -20,21 +32,21 @@ typedef struct s_inter
 	double		angle;
 	int			origin_offset;
 	double		wall_height;
-} t_inter;
+}				t_inter;
 
 //calc_interseciton.c
-t_inter calc_intersection(t_mlx *mlx, double ray_angle);
+t_inter			calc_intersection(t_mlx *mlx, double ray_angle);
 
 //calc_utils.c
-double sin_wrap(double angle);
-double cos_wrap(double angle);
-double tan_wrap(double angle);
-double cot_wrap(double angle);
+double			sin_wrap(double angle);
+double			cos_wrap(double angle);
+double			tan_wrap(double angle);
+double			cot_wrap(double angle);
 
 //calc_utils2.c
-double fix_angle(double angle);
-t_pos	fix_move(t_mlx *mlx, t_pos pos, double move_angle, int step);
-bool	is_up(double degree);
-bool	is_right(double degree);
+double			fix_angle(double angle);
+t_pos			fix_move(t_mlx *mlx, t_pos pos, double move_angle, int step);
+bool			is_up(double degree);
+bool			is_right(double degree);
 
 #endif
