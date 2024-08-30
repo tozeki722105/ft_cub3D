460eb2cb9b3efa561993f674b73efb4562a6d885
taka-chin <ty80164@gmail.com>
2024-08-31 04:44:53 +0900

    norm対応

--- srcs/mlx_wrapper/ft_x_mlx_get_data_addr.c
+++ srcs/mlx_wrapper/ft_x_mlx_get_data_addr.c
@@ -1,12 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_x_mlx_get_data_addr.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tyamauch <tyamauch@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/31 04:43:33 by tyamauch          #+#    #+#             */
+/*   Updated: 2024/08/31 04:43:35 by tyamauch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 #include "mlx.h"
 #include "utils.h"
 
-void *ft_x_mlx_get_data_addr(void *img_ptr, int *bits_per_pixel,int *size_line, int *endian)
+void	*ft_x_mlx_get_data_addr(void *img_ptr, int *bits_per_pixel,
+		int *size_line, int *endian)
 {
-  void	*ret;
+	void	*ret;
 
-	ret = mlx_get_data_addr(img_ptr,bits_per_pixel,size_line,endian);
+	ret = mlx_get_data_addr(img_ptr, bits_per_pixel, size_line, endian);
 	if (ret == NULL)
 		ft_my_perror_exit("mlx_get_data_addr error", 1);
 	return (ret);
--- srcs/mlx_wrapper/ft_x_mlx_xpm_file_to_image.c
+++ srcs/mlx_wrapper/ft_x_mlx_xpm_file_to_image.c
@@ -1,9 +1,14 @@
-//linux
-// void	*mlx_xpm_file_to_image(t_xvar *xvar,char *filename,int *width,int *height)
-
-//mac
-// void	*mlx_xpm_file_to_image(mlx_ptr_t *xvar,char *file,int *width,int *height)
-
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_x_mlx_xpm_file_to_image.c                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tyamauch <tyamauch@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/31 04:38:21 by tyamauch          #+#    #+#             */
+/*   Updated: 2024/08/31 04:38:30 by tyamauch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "libft.h"
 #include "mlx.h"
@@ -11,22 +16,24 @@
 
 #if defined(__linux__)
 
-void	*ft_x_mlx_xpm_file_to_image(t_xvar *xvar,char *filename, int *width,int *height)
+void	*ft_x_mlx_xpm_file_to_image(t_xvar *xvar, char *filename, int *width,
+		int *height)
 {
 	void	*ret;
 
-	ret = mlx_xpm_file_to_image(xvar,filename,width,height);
+	ret = mlx_xpm_file_to_image(xvar, filename, width, height);
 	if (ret == NULL)
 		ft_my_perror_exit("mlx_xpm_file_to_image error", 1);
 	return (ret);
 }
 #else
 
-void *ft_x_mlx_xpm_file_to_image(void *mlx_ptr,char *filename,int *width,int *height)
+void	*ft_x_mlx_xpm_file_to_image(void *mlx_ptr, char *filename, int *width,
+		int *height)
 {
-  void	*ret;
+	void	*ret;
 
-	ret = mlx_xpm_file_to_image(mlx_ptr,filename,width,height);
+	ret = mlx_xpm_file_to_image(mlx_ptr, filename, width, height);
 	if (ret == NULL)
 		ft_my_perror_exit("mlx_xpm_file_to_image error", 1);
 	return (ret);
